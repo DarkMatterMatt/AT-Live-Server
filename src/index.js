394ab3521bf38bebd41b04d118f06d3ffc661041
Matt Moran <15150339+DarkMatterMatt@users.noreply.github.com>
2020-03-19 01:23:42 +1300

    Working get API for shortNames

--- src/index.js
+++ src/index.js
@@ -1,19 +1,55 @@
 const uWS = require("uWebSockets.js");
+const AucklandTransportData = require("./AucklandTransportData");
 const C = require("./config");
 
 const app = C.useSSL ? uWS.SSLApp(C.ssl) : uWS.App();
 
 /* Subscribe / publish by route short names */
+const aucklandTransportData = new AucklandTransportData(C.aucklandTransport.key, C.aucklandTransport.baseUrl);
 
-app.ws("/v1/", {
-    ...C.ws.v1.opts,
+(async () => {
+    await aucklandTransportData.lookForUpdates("forceLoad");
 
-    message: (ws, message, isBinary) => {
-        const ok = ws.send(message, isBinary);
-    },
-});
-app.listen(9001, (listenSocket) => {
-    if (listenSocket) {
-        console.log("Listening to port 9001");
-    }
-});
+    app.ws("/v1/", {
+        ...C.ws.v1.opts,
+
+        message: (ws, message, isBinary) => {
+            const ok = ws.send(message, isBinary);
+        },
+    });
+
+    app.get("/v1/:shortName", (res, req) => {
+        const shortName = req.getParameter(0).toUpperCase();
+        res.writeHeader("Content-Type", "application/json");
+
+        const route = aucklandTransportData.getRouteByShortName(shortName);
+
+        if (!route) {
+            res.end(JSON.stringify({
+                status: "error",
+                error:  "Specified route does not exist.",
+            }));
+            return;
+        }
+
+        const { polylines } = route;
+        const vehicles = [...route.vehicles.values()];
+
+        res.end(JSON.stringify({
+            status: "success",
+            shortName,
+            polylines,
+            vehicles,
+        }));
+    });
+
+    app.any("/*", (res, req) => {
+        res.end("Invalid endpoint");
+    });
+
+    app.listen(9001, (listenSocket) => {
+        if (listenSocket) {
+            console.log("Listening to port 9001");
+        }
+    });
+})();
