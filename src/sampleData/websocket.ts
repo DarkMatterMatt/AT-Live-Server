b1632c80fe67dfbfe4b960c5077f763fe60f48e0
DarkMatterMatt <mattmoran2000@gmail.com>
2021-02-06 13:20:32 +1300

    Update sample websocket data

--- src/sampleData/websocket.ts
+++ src/sampleData/websocket.ts
@@ -4,14 +4,23 @@ export const WEBSOCKET_VEHICLE_UPDATE: {
     vehicle: {
         vehicle: {
             id: "31359",
+            label: "NB5012",
+            licensePlate: "JUM511",
         },
         trip: {
+            tripId: "51438209768-20200702150240_v89.131",
+            startTime: "23:25:00",
+            startDate: "20200713",
+            scheduleRelationship: "SCHEDULED",
             routeId: "20008-20201126121446_v95.73",
             directionId: 1,
         },
         position: {
             latitude: -36.722318333333334,
             longitude: 174.711945,
+            bearing: 115,
+            odometer: 182562115,
+            speed: 9.722222222222222,
         },
         timestamp: "1607156277",
         occupancyStatus: "MANY_SEATS_AVAILABLE",
