b4fe5cd4a73fa0b0fae37beb5019feddaaf3a9a4
Matt Moran <mattmoran2000@gmail.com>
2021-11-14 22:18:31 +1300

    Update to use ES Modules

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,39 +21,40 @@ module.exports = {
         "arrow-spacing": "warn",
         "brace-style": ["warn", "stroustrup"],
         "comma-dangle": ["warn", {
-            "arrays": "always-multiline",
-            "objects": "always-multiline",
-            "imports": "always-multiline",
-            "exports": "always-multiline",
-            "functions": "never",
+            arrays: "always-multiline",
+            objects: "always-multiline",
+            imports: "always-multiline",
+            exports: "always-multiline",
+            functions: "never",
         }],
         "eqeqeq": ["error", "smart"],
         "@typescript-eslint/indent": ["warn", 4, {
-            "SwitchCase": 1,
+            SwitchCase: 1,
         }],
         "keyword-spacing": "warn",
         "max-len": ["warn", {
-            "code": 120,
-            "comments": 100,
-            "ignorePattern": "^\\s*import.*from.*;$", // ignore long imports
+            code: 120,
+            comments: 100,
+            ignorePattern: "^\\s*import.*from.*;$", // ignore long imports
         }],
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/no-inferrable-types": ["warn", {
-            "ignoreParameters": true,
+            ignoreParameters: true,
         }],
         "no-mixed-operators": "error",
         "no-trailing-spaces": "warn",
         "@typescript-eslint/no-unused-vars": ["error", {
-            "argsIgnorePattern": "_+",
-            "varsIgnorePattern": "_+",
+            argsIgnorePattern: "_+",
+            varsIgnorePattern: "_+",
         }],
         "object-curly-spacing": ["warn", "always"],
         "operator-linebreak": ["warn", "after"],
         "prefer-destructuring": "warn",
         "prefer-template": "warn",
         "quotes": ["warn", "double", {
-            "avoidEscape": true,
+            avoidEscape: true,
         }],
+        "quote-props": ["error", "consistent-as-needed"],
         "semi": ["error", "always"],
     },
 };
