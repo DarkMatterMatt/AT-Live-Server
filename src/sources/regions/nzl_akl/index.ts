94b66d333945735939d2f5d4f34b658ed8e34512
Matt Moran <mattmoran2000@gmail.com>
2021-11-22 01:42:26 +1300

    Fix incorrect type name

--- src/sources/regions/nzl_akl/index.ts
+++ src/sources/regions/nzl_akl/index.ts
@@ -1,10 +1,10 @@
-import type { RegionCode, Source } from "~/types";
+import type { RegionCode, DataSource } from "~/types";
 import { getVehicles, registerTripUpdateListener, registerVehicleUpdateListener } from "./realtime.js";
 import { checkForStaticUpdate, getShapesByShortName, getShortName, getTripId } from "./static.js";
 
 const regionCode: RegionCode = "NZL_AKL";
 
-export const NZL_AKL: Source = {
+export const NZL_AKL: DataSource = {
     code: regionCode,
 
     checkForStaticUpdate,
