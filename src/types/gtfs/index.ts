b5d083ecd2e4a04b8ff6063ea48827cbefb3752d
Matt Moran <mattmoran2000@gmail.com>
2022-10-28 11:56:11 +1300

    Update import/export syntax

--- src/types/gtfs/index.ts
+++ src/types/gtfs/index.ts
@@ -1,10 +1,10 @@
 export * from "./gtfs-realtime.js";
 
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import * as RT from "./gtfs-realtime.js";
-export import Translation = RT.TranslatedString$Translation;
-export import StopTimeEvent = RT.TripUpdate$StopTimeEvent;
-export import StopTimeUpdate = RT.TripUpdate$StopTimeUpdate;
-export import CongestionLevel = RT.VehiclePosition$CongestionLevel;
-export import OccupancyStatus = RT.VehiclePosition$OccupancyStatus;
-export import VehicleStopStatus = RT.VehiclePosition$VehicleStopStatus;
+export {
+    type TranslatedString$Translation as Translation,
+    type TripUpdate$StopTimeEvent as StopTimeEvent,
+    type TripUpdate$StopTimeUpdate as StopTimeUpdate,
+    VehiclePosition$CongestionLevel as CongestionLevel,
+    VehiclePosition$OccupancyStatus as OccupancyStatus,
+    VehiclePosition$VehicleStopStatus as VehicleStopStatus,
+} from "./gtfs-realtime.js";
