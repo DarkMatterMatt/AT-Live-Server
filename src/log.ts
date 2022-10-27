678385b1e1132d52d4f5d411aa08f611be9c3133
Matt Moran <mattmoran2000@gmail.com>
2022-10-27 23:50:47 +1300

    Simplify logger

--- src/log.ts
+++ src/log.ts
@@ -1,19 +1,21 @@
-import chalk from "chalk";
-import { createLogger, format, transports } from "winston";
-import { inspect } from "util";
-import { SPLAT } from "triple-beam";
+import { inspect } from "node:util";
+import chalk, { type Chalk } from "chalk";
 import jsonStringify_ from "fast-safe-stringify";
+import { type TransformableInfo } from "logform";
+import { createLogger, format, transports } from "winston";
 import "winston-daily-rotate-file";
 
-const colors = {
-    error: "redBright",
-    warn: "yellowBright",
-    info: "greenBright",
-    http: "greenBright",
-    verbose: "cyanBright",
-};
+const logLevels = ["debug", "verbose", "info", "warn", "error"] as const;
 
-type Colour = keyof typeof colors;
+type LogLevel = typeof logLevels[number];
+
+const colors: { [K in LogLevel]: Chalk } = {
+    error: chalk.bold.redBright,
+    warn: chalk.bold.yellowBright,
+    info: chalk.bold.greenBright,
+    verbose: chalk.bold.cyanBright,
+    debug: chalk.bold.whiteBright,
+};
 
 const isPrimitive = (val: any) => val === null || (typeof val !== "object" && typeof val !== "function");
 
@@ -21,30 +23,34 @@ const formatWithInspect = (val: any): string => {
     if (typeof val === "string") {
         return val;
     }
+    const errorPrefix = val instanceof Error ? val.toString() : "";
     const newLine = isPrimitive(val) ? "" : "\n";
-    return newLine + inspect(val, { depth: null, colors: true });
-};
-
-export const colorize = (color: string, s: string): string => {
-    if (!color) {
-        return s;
-    }
-    // Chalk is designed to be indexed by a string
-    const bold = (chalk.bold as unknown) as Record<string, (s: string) => string>;
-    if (typeof bold[color] !== "function") {
-        throw new Error(`Invalid console text color: '${color}'`);
-    }
-    return bold[color](s);
+    return errorPrefix + newLine + inspect(val, { depth: null, colors: true });
 };
 
 const jsonStringifyErrors = (_key: string, value: any) => {
     if (value instanceof Error) {
-        return Object.fromEntries(Object.entries(value));
+        const { name, message, stack } = value;
+        return { name, message, stack };
     }
     return value;
 };
 
 const jsonStringify = (obj: any) => jsonStringify_(obj, jsonStringifyErrors);
+
+const EXTRA_DATA = Symbol("EXTRA_DATA");
+
+interface ExtraData {
+    label: string;
+    moreObjects: any[];
+}
+
+const extractData = (info: TransformableInfo) => ({
+    ...info[EXTRA_DATA as any] as ExtraData,
+    message: info.message,
+    level: info.level as LogLevel,
+    timestamp: info.timestamp as string,
+});
 
 const log = createLogger({
     transports: [
@@ -54,37 +60,29 @@ const log = createLogger({
             maxFiles: "14d",
             format: format.combine(
                 format.timestamp(),
-                format.errors({ stack: true }),
-                format.printf(info => {
-                    // SPLAT is always a valid index for TransformableInfo
-                    const splatArgs = info[SPLAT as any];
-                    if (splatArgs) {
-                        const messagesArr = [info.message].concat(splatArgs);
-                        // this is NOT a string but jsonStringify isn't fussy
-                        info.message = messagesArr as any;
-                    }
-                    return jsonStringify(info);
-                }),
+                format.printf(info => jsonStringify(extractData(info))),
             ),
         }),
         new transports.Console({
-            level:  "debug",
+            level: "debug",
             format: format.combine(
                 format.timestamp({ format: "MMM DD, HH:mm:ss" }),
                 format.printf(info => {
-                    const label: string = info.label ?? "";
-                    const coloredLabelLevel = colorize(colors[info.level as Colour], `${label}:${info.level}`);
-                    const msg = formatWithInspect(info.message);
-                    // SPLAT is always a valid index for TransformableInfo
-                    const splatArgs = info[(SPLAT as unknown) as string] || [];
-                    const rest = splatArgs.map(formatWithInspect).join(" ");
-                    return `${info.timestamp}  ${coloredLabelLevel}  ${msg} ${rest}`;
+                    const { label, level, timestamp, message, moreObjects } = extractData(info);
+                    const coloredLabelLevel = colors[level](`${label}:${level}`);
+                    const formattedObjs = moreObjects.map(formatWithInspect).join(" ");
+                    return [timestamp, coloredLabelLevel, message, moreObjects.length ? formattedObjs : ""].join("  ");
                 }),
             ),
         }),
     ],
 });
 
-export function getLogger(label: string) {
-    return log.child({ label });
-}
+const getLoggerLevel = (label: string, level: LogLevel) => (message: string, ...moreObjects: any[]) => {
+    const extractData: ExtraData = { label, moreObjects };
+    log.log(level, message, { [EXTRA_DATA]: extractData });
+};
+
+export const getLogger = (label: string) => Object.freeze(Object.fromEntries(
+    logLevels.map(lvl => [lvl, getLoggerLevel(label, lvl)]),
+) as { [K in LogLevel]: ReturnType<typeof getLoggerLevel> });
