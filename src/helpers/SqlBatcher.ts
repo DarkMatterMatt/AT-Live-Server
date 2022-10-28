3906344e5e59585b94e73d6d8a58306d6d5d844d
Matt Moran <mattmoran2000@gmail.com>
2022-10-28 18:34:54 +1300

    Automatically determine number of parameters supported

--- src/helpers/SqlBatcher.ts
+++ src/helpers/SqlBatcher.ts
@@ -1,11 +1,13 @@
-import { SqlDatabase } from "gtfs/@types";
+import { SqlDatabase } from "gtfs";
+import { getLogger } from "~/log";
 
-const MAX_INSERT_VARIABLES = 800;
+const log = getLogger("SQLBatcher");
 
 export interface SqlBatcherOpts<T extends any[]>{
     db: SqlDatabase;
     table: string;
     columns: { [K in keyof T]: string }; // array of strings, same length as T
+    SQLITE_MAX_VARIABLE_NUMBER?: number;
 }
 
 /**
@@ -18,12 +20,58 @@ export class SqlBatcher<T extends any[]>{
     private readonly columns: { [K in keyof T]: string };
     private readonly placeholders: any[] = [];
     private readonly values: any[] = [];
+    private readonly initialized: Promise<void>;
+    private maxVariables = -1;
 
     /** Create a new SqlBatcher. Values will be inserted into the specified table columns. */
-    public constructor({ db, table, columns }: SqlBatcherOpts<T>) {
+    public constructor({
+        db,
+        table,
+        columns,
+        SQLITE_MAX_VARIABLE_NUMBER,
+    }: SqlBatcherOpts<T>) {
         this.db = db;
         this.table = table;
         this.columns = columns;
+
+        if (SQLITE_MAX_VARIABLE_NUMBER != null) {
+            this.maxVariables = SQLITE_MAX_VARIABLE_NUMBER;
+        }
+
+        this.initialized = new Promise(r =>
+            this.init()
+                .then(r)
+                .catch(err => {
+                    log.error("Failed to initialize SqlBatcher", err);
+                    throw err;
+                }));
+    }
+
+    private async init() {
+        if (this.maxVariables === -1) {
+            this.maxVariables = await this.findMaxVariables();
+        }
+    }
+
+    private async findMaxVariables() {
+        // Search for MAX_VARIABLE_NUMBER compile option.
+        const compileOptions = await this.db.all<{ compile_options: string }[]>("PRAGMA compile_options");
+        const maxVariables = compileOptions
+            .map(row => row.compile_options)
+            .find(row => row.startsWith("MAX_VARIABLE_NUMBER="));
+        if (maxVariables != null) {
+            return Number.parseInt(maxVariables.split("=")[1]);
+        }
+
+        // Default is 999 before v3.32.0 (2020-05-22), and 32766 from v3.32.0.
+        const sqliteVersion = await this.db.get("SELECT sqlite_version() AS version") as { version: string };
+        const parts = sqliteVersion.version
+            .split(".")
+            .map(part => Number.parseInt(part));
+        if (parts.length === 3 && (parts[0] > 3 || (parts[0] === 3 && parts[1] >= 32))) {
+            return 32766;
+        }
+        return 999;
     }
 
     private async actionBatch() {
@@ -40,16 +88,24 @@ export class SqlBatcher<T extends any[]>{
     }
 
     public async queue(...items: T) {
+        await this.initialized;
+
+        if (this.columns.length !== items.length) {
+            log.error("Incorrect number of items", { expected: this.columns, received: items });
+            throw new Error(`Expected ${this.columns.length} items, got ${items.length}`);
+        }
+
         this.placeholders.push(`(${this.columns.map(() => "?").join(", ")})`);
         this.values.push(...items);
 
         // have filled up this batch, insert them now
-        if (this.values.length + items.length > MAX_INSERT_VARIABLES) {
+        if (this.values.length + items.length > await this.maxVariables) {
             await this.actionBatch();
         }
     }
 
     public async flush() {
-        this.actionBatch();
+        await this.initialized;
+        await this.actionBatch();
     }
 }
